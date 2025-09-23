1b3a35067266df62afc69def7cded87d832db616
Haris-Rahat <137375469+Haris-Rahat@users.noreply.github.com>
2025-09-23 16:27:02 +0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let files: FileList | null = $state(null);
+
+  $effect(() => {
+    console.log(files, "files");
+  });
+</script>
+
+<main>
+  <div>
+    <p>Upload File</p>
+  </div>
+  <div class="card">
+    <input bind:files type="file" />
+  </div>
+</main>
+
+<style>
+</style>
